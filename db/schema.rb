d2a031f89c6ec2993d283dda542492a55866c1b4
Alexandra Doll <alexandra.doll100@gmail.com>
2018-05-22 11:59:00 +0900

    hjhkjh

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_014849) do
+ActiveRecord::Schema.define(version: 2018_05_22_022445) do
 
   create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
